e9ecfe5fea6d0692490a90049f04c82fe2a7bcb4
cjwind <cwentsai@gmail.com>
2019-07-09 23:37:40 +0800

    Convert list

--- doku2md.php
+++ doku2md.php
@@ -1,4 +1,18 @@
 <?php
+function convertUnorderedList($line): string
+{
+    $line = preg_replace('/  \*( *)(.*)/', '* ${2}', $line);
+    $spaceCount = strpos($line, '*');
+    return str_repeat(' ', $spaceCount) . $line;
+}
+
+function convertOrderedList($line): string
+{
+    $line = preg_replace('/  -( *)(.*)/', '1. ${2}', $line);
+    $spaceCount = strpos($line, '1. ');
+    return str_repeat(' ', $spaceCount) . $line;
+}
+
 function doku2md($str)
 {
     $lines = explode("\n", $str);
@@ -15,6 +29,9 @@ function doku2md($str)
         foreach ($patternAndReplacements as $pattern => $replacement) {
             $line = preg_replace($pattern, $replacement, $line);
         }
+
+        $line = convertUnorderedList($line);
+        $line = convertOrderedList($line);
     }
 
     return implode("\n", $lines);
